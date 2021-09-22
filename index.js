d3778dc21d352402d2e64f2d69998916e9d801fd
David <davidchedrick@gmail.com>
2021-09-21 22:35:55 -0500

    commit tree ec4c047e6ae6d740e9454938a48cf6912c80031b

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i=0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i ;
+    const startChar = word[i];
+    const endChar = word[j];
+
+    if (startChar !== endChar) return false;
+  }
+  return true
 }
 
 /* 
+r a c e c a r
+0 1 2 3 4 5 6
+i           j
+
+r a c e c a r
+0 1 2 3 4 5 6
+  i       j
+
+r a c e c a r
+0 1 2 3 4 5 6
+    i   j
+
+r a c e c a r
+0 1 2 3 4 5 6
+      ij
+
   Add your pseudocode here
+  function, returns true is a paliindrome
+
+  if first letter same as last letter, second is same as second to last, same until middle letter
+
+  abba
+  ^  ^
+   ^^
+  true
+
+  robot
+  ^   ^
+  false
+
+  itterate from beginning to  middle, 
+  check corrisponding letter 
+  if any letter dont match, not a pali
+
 */
 
 /*
